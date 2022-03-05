119059e96677f2b9360cd921d3afe0bc75da76c5
zeekrs <git@zeekrs.dev>
2022-03-05 21:25:31 +0800

    chore: nvim options

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -10,6 +10,15 @@ o.tabstop = 2
 o.cursorline = true
 o.number = true
 o.numberwidth = 4
+o.splitbelow = true
+o.splitright = true
+o.termguicolors = true
+o.wrap = false
+o.scrolloff = 8
+o.sidescrolloff = 8
+
+
+
 
 
 
