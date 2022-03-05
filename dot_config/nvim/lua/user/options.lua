805370b41d8628b0fe7119ca054968f363d5e5a9
zeekrs <git@zeekrs.dev>
2022-03-05 12:49:47 +0800

    first ccommit

---
+++ dot_config/nvim/lua/user/options.lua
@@ -0,0 +1,16 @@
+local o = vim.o
+
+o.clipboard = "unnamedplus"
+o.cmdheight = 2
+o.fileencoding = "utf-8"
+o.smartcase = true
+o.smartindent = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.cursorline = true
+o.number = true
+o.numberwidth = 4
+
+
+
+
