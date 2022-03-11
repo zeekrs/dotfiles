2255d69c5838be207bd513baaf985bd84175d8d6
zeekrs <git@zeekrs.dev>
2022-03-11 15:23:38 +0800

    chore: nvim config

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -99,6 +99,9 @@ return packer.startup(function(use)
 
 	use("akinsho/toggleterm.nvim")
 
+	--Markdown Preview
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install", ft = "markdown" })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
