3eeba5b7da752666ac614b68d0daf813a0aa9138
zeekrs <git@zeekrs.dev>
2022-03-17 23:40:08 +0800

    config: neovim lualine

--- dot_config/nvim/lua/user/lualine.lua
+++ dot_config/nvim/lua/user/lualine.lua
@@ -1,4 +1,5 @@
 local colors = require("kanagawa.colors").setup()
+
 local conditions = {
 	buffer_not_empty = function()
 		return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
@@ -7,32 +8,8 @@ local conditions = {
 		return vim.fn.winwidth(0) > 80
 	end,
 }
-local mode_color = function()
-	-- auto change color according to neovims mode
-	local mode_color = {
-		n = colors.crystalBlue,
-		i = colors.autumnGreen,
-		v = colors.oniViolet,
-		V = colors.oniViolet,
-		c = colors.boatYellow2,
-		no = colors.crystalBlue,
-		s = colors.sakuraPink,
-		S = colors.sakuraPink,
-		[""] = colors.sakuraPink,
-		ic = colors.lightBlue,
-		R = colors.sakuraPink,
-		Rv = colors.sakuraPink,
-		cv = colors.crystalBlue,
-		ce = colors.crystalBlue,
-		r = colors.waveRed,
-		rm = colors.waveRed,
-		["r?"] = colors.waveRed,
-		["!"] = colors.crystalBlue,
-		t = colors.crystalBlue,
-	}
-	return { fg = mode_color[vim.fn.mode()] }
-end
 
+-- diff_source from gitsigns
 local function diff_source()
 	local gitsigns = vim.b.gitsigns_status_dict
 	if gitsigns then
@@ -43,6 +20,7 @@ local function diff_source()
 		}
 	end
 end
+-- flatten null-ls support sources
 local function null_ls_support_source_names(fileType, method)
 	local sources = require("null-ls.sources")
 	local availableSources = sources.get_available(fileType)
@@ -57,118 +35,54 @@ local function null_ls_support_source_names(fileType, method)
 	return source_names
 end
 
-local config = {
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {},
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {},
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
-}
+-- components
 
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-	table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x ot right section
-local function ins_right(component)
-	table.insert(config.sections.lualine_x, component)
-end
-
-ins_left({
+local start_block = {
 	function()
 		return "▊"
 	end,
-	color = mode_color, -- Sets highlighting of component
 	padding = { left = 0, right = 1 }, -- We don't need space before this
-})
+	color = { bg = colors.bg_light0 },
+}
 
-ins_left({
-	-- mode component
+local mode = {
 	function()
 		return ""
 	end,
-	color = mode_color,
 	padding = { right = 1 },
-})
+	color = { bg = colors.bg_light0 },
+}
 
-ins_left({
+local filename = {
 	"filename",
 	cond = conditions.buffer_not_empty,
-	color = { fg = colors.oniViolet, gui = "bold" },
-})
+	color = { gui = "bold", bg = colors.bg_light0 },
+}
 
-ins_left({
-	-- filesize component
-	"filesize",
-	cond = conditions.buffer_not_empty,
-})
+local location = { "location", color = { gui = "bold" } }
 
-ins_left({ "location", color = { gui = "bold" } })
+local progress = { "progress" }
 
-ins_left({ "progress" })
-
-ins_left({
+local diagnostics = {
 	"diagnostics",
 	sources = { "nvim_diagnostic" },
 	symbols = { error = " ", warn = " ", info = " ", hint = " " },
-	diagnostics_color = {
-		color_error = { fg = colors.samuraiRed },
-		color_warn = { fg = colors.roninYellow },
-		color_info = { fg = colors.waveAqua1 },
-	},
-})
-
--- Insert mid section. You can make any number of sections in neovim :)
--- for lualine it's any number greater then 2
-ins_left({
-	function()
-		return "%="
-	end,
-})
-
--- Add components to right sections
-ins_right({
+}
+local diff = {
 	"diff",
 	source = diff_source,
 	symbols = { added = "  ", modified = " ", removed = " " },
-	diff_color = {
-		added = { fg = colors.autumnGreen },
-		modified = { fg = colors.autumnYellow },
-		removed = { fg = colors.autumnRed },
-	},
 	cond = conditions.hide_in_width,
-})
+}
 
-ins_right({
+local branch = {
 	"b:gitsigns_head",
 	icon = " ",
 	color = { gui = "bold" },
 	cond = conditions.hide_in_width,
-})
+}
 
-ins_right({
+local lsp = {
 	function()
 		local msg = "No Active Lsp"
 		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
@@ -201,9 +115,9 @@ ins_right({
 	icon = " LSP:",
 	color = { gui = "bold" },
 	cond = conditions.hide_in_width,
-})
+}
 
-ins_right({
+local treesitter = {
 	function()
 		local b = vim.api.nvim_get_current_buf()
 		if next(vim.treesitter.highlighter.active[b]) then
@@ -213,42 +127,61 @@ ins_right({
 	end,
 	color = { fg = colors.springGreen },
 	cond = conditions.hide_in_width,
-})
+}
 
-ins_right({
-	function()
-		if not vim.api.nvim_buf_get_option(0, "expandtab") then
-			return "Tab size: " .. vim.api.nvim_buf_get_option(0, "tabstop") .. " "
-		end
-		local size = vim.api.nvim_buf_get_option(0, "shiftwidth")
-		if size == 0 then
-			size = vim.api.nvim_buf_get_option(0, "tabstop")
-		end
-		return "Spaces: " .. size .. " "
-	end,
-	cond = conditions.hide_in_width,
-	color = {},
-})
-ins_right({
+local filesize = {
+	"filesize",
+	cond = conditions.buffer_not_empty,
+}
+local encoding = {
 	"o:encoding", -- option component same as &encoding in viml
 	fmt = string.upper, -- I'm not sure why it's upper case either ;)
 	cond = conditions.hide_in_width,
-	color = { fg = colors.springGreen, gui = "bold" },
-})
+	color = { bg = colors.bg_light0, gui = "bold" },
+}
 
-ins_right({
+local fileformat = {
 	"fileformat",
 	fmt = string.upper,
 	icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-	color = { fg = colors.springGreen, gui = "bold" },
-})
+	color = { bg = colors.bg_light0, gui = "bold" },
+}
 
-ins_right({
+-- end with  block
+local end_block = {
 	function()
 		return "▊"
 	end,
-	color = mode_color,
-	padding = { left = 1 },
-})
+	padding = { left = 1, right = 0 }, -- We don't need space before this
 
-require("lualine").setup(config)
+	color = { bg = colors.bg_light0 },
+}
+
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {},
+		lualine_b = { start_block, mode, filename },
+		lualine_c = { location, progress, diagnostics },
+		lualine_x = { diff, branch, lsp, treesitter, filesize },
+		lualine_y = { encoding, fileformat, end_block },
+		lualine_z = {},
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
