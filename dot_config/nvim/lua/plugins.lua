535ea603127f66b4583b10a4ddbef09ce680e132
zeekrs <git@zeekrs.dev>
2022-03-24 00:20:49 +0800

    config: nvim config

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,6 +2,9 @@ local fn = vim.fn
 
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+local packer_bootstrap
+
 if fn.empty(fn.glob(install_path)) > 0 then
 	packer_bootstrap = fn.system({
 		"git",
