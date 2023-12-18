3fc209f522b30fd6b10e279b9808d5e80becda23
zeekrs <git@zeekrs.dev>
2023-12-18 22:50:01 +0800

    replace comment plugin & refactory file names

---
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,101 @@
+return {
+  -- override cmp keymaps
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      -- auto pairs <cr>
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+      fast_wrap = {},
+    },
+    keys = {
+      {
+        "<leader>up",
+        function()
+          local Util = require("lazy.core.util")
+          local autopairs = require("nvim-autopairs")
+          local disabled = autopairs.state.disabled
+          if disabled then
+            Util.info("Enabled auto pairs", { title = "Option" })
+            autopairs.enable()
+          else
+            Util.warn("Disabled  auto pairs", { title = "Option" })
+            autopairs.disable()
+          end
+        end,
+        desc = "Toggle auto pairs",
+      },
+    },
+  },
+  {
+    "echasnovski/mini.comment",
+    enabled = false,
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    },
+    event = "VeryLazy",
+  },
+  {
+    "danymat/neogen",
+    event = "VeryLazy",
+    opts = {
+      snippet_engine = "luasnip",
+    },
+    keys = {
+      {
+        "<leader>cg",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen",
+      },
+    },
+  },
+}
