409bab81cc8276e756da9f9726a757dc75fe3b5e
zeekrs <git@zeekrs.dev>
2023-12-20 13:30:33 +0800

    upgrade harpoon to harpoon2

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -3,6 +3,7 @@ local term_string = vim.fn.exists("$TMUX") == 1 and "tmux" or "terminal"
 local maps = { n = {} }
 local icon = vim.g.icons_enabled and "󱡀 " or ""
 maps.n[prefix] = { desc = icon .. "Harpoon" }
+
 return {
 
   {
@@ -17,62 +18,52 @@ return {
 
   {
     "ThePrimeagen/harpoon",
-    lazy = true,
+    branch = "harpoon2",
+    config = function(_, opts)
+      require("harpoon"):setup(opts)
+    end,
     keys = {
       {
         prefix .. "a",
         function()
-          require("harpoon.mark").add_file()
+          require("harpoon"):list():append()
         end,
         desc = "Add file",
       },
       {
+        prefix .. "d",
+        function()
+          require("harpoon"):list():remove()
+        end,
+        desc = "Remove file",
+      },
+      {
+        prefix .. "c",
+        function()
+          require("harpoon"):list():clear()
+        end,
+        desc = "Clear files",
+      },
+      {
         prefix .. "e",
         function()
-          require("harpoon.ui").toggle_quick_menu()
+          require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
         end,
         desc = "Toggle quick menu",
       },
-      -- fix key
       {
         prefix .. "g",
         function()
           vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
             local num = tonumber(input)
             if num then
-              require("harpoon.ui").nav_file(num)
+              require("harpoon"):list():select(num)
             end
           end)
         end,
         desc = "Goto index of mark",
       },
-      {
-        "<C-p>",
-        function()
-          require("harpoon.ui").nav_prev()
-        end,
-        desc = "Goto previous mark",
-      },
-      {
-        "<C-n>",
-        function()
-          require("harpoon.ui").nav_next()
-        end,
-        desc = "Goto next mark",
-      },
       { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
-      {
-        prefix .. "t",
-        function()
-          vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
-            local num = tonumber(input)
-            if num then
-              require("harpoon." .. term_string).gotoTerminal(num)
-            end
-          end)
-        end,
-        desc = "Go to " .. term_string .. " window",
-      },
     },
   },
 }
