bb4e4864bc9bbaab8bd9c816e28acde0b55ababd
zeekrs <git@zeekrs.dev>
2023-12-20 17:28:58 +0800

    remove cro for format options

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,6 @@
 if vim.fn.executable("volta") then
   vim.g.node_host_prog = vim.fn.trim(vim.fn.system("volta which neovim-node-host"))
 end
+
+-- disabled comment formatoptions
+vim.opt.formatoptions = "jqlnt" -- tcqj
