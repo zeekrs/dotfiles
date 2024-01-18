130f37eefa2c44be9ff8a7dfa818ea395013b055
zeekrs <git@zeekrs.dev>
2024-01-18 20:55:30 +0800

    add dial pattern

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -202,6 +202,25 @@ return {
   -- Better increase/descrease
   {
     "monaqa/dial.nvim",
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.integer.alias.binary,
+          augend.date.alias["%Y/%m/%d"],
+          augend.date.alias["%Y-%m-%d"],
+          augend.date.alias["%m/%d"],
+          augend.date.alias["%H:%M"],
+          augend.date.alias["%Y年%-m月%-d日"],
+          augend.constant.alias.bool,
+          augend.constant.alias.alpha,
+          augend.constant.alias.Alpha,
+          augend.constant.alias.semver,
+        },
+      })
+    end,
     keys = {
       {
         "<C-a>",
