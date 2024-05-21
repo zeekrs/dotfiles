4fc58dbb149e6d7ad039f33265c1d110924579af
zeekrs <git@zeekrs.dev>
2024-05-21 11:39:41 +0800

    enable luasnip

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,7 @@
 return {
   -- override cmp keymaps
+  --
+  --
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
@@ -8,6 +10,7 @@ return {
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       -- auto pairs <cr>
@@ -20,10 +23,8 @@ return {
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
             -- this way you will only jump inside the snippet region
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
           else
@@ -33,8 +34,8 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.snippet.jump(-1)
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
