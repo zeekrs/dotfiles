fa975baefbda1ec9160d9f28d189c2f6c8af2b98
zeekrs <git@zeekrs.dev>
2024-07-22 13:58:15 +0800

    feat(fish): add jetbrains idea in path

--- dot_config/fish/conf.d/paths.fish
+++ dot_config/fish/conf.d/paths.fish
@@ -42,3 +42,6 @@ fish_add_path "$ANDROID_HOME/platform-tools"
 
 # ruby 
 fish_add_path /opt/homebrew/opt/ruby/bin
+
+#jet brains ide
+fish_add_path "/Users/zeekrs/Library/Application Support/JetBrains/Toolbox/scripts"
