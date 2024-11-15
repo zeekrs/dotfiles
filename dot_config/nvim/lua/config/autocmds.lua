1162d2df19279138599b2659ba62a20fe4e21ec1
zeekrs <git@zeekrs.dev>
2024-11-15 16:33:41 +0800

    feat(nvim): update version to 7 and add autocmd for justfile filetype

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "justfile" },
+  callback = function()
+    vim.cmd([[setlocal filetype=make]])
+  end,
+})
